065d30859d333370dd2bf3404740ef13e13e8411
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:23 +0100

    improve

--- liguedelorainne/client/src/Components/DescriptionBox/DescriptionBox.jsx
+++ liguedelorainne/client/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import './DescriptionBox.css'
 
-const DescriptionBox = () => {
+const DescriptionBox = ({ description, reviewsCount, details, onSeeDetailsClick }) => {
   return (
     <div className='descriptionbox'>
       <div className="descriptionbox-navigator">
-        <div className="descriptionbox-nav-box">Description</div>
-        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        <div className={`descriptionbox-nav-box ${!details ? 'descriptionbox-nav-box-selected' : ''}`}>Description</div>
+        <div className={`descriptionbox-nav-box fade ${details ? 'descriptionbox-nav-box-selected' : ''}`}>Reviews ({reviewsCount})</div>
       </div>
       <div className="descriptionbox-description">
-        <p>
-          </p>
+        {
+          description ? (
+            <p>{description}</p>
+          ) : (
+            <p className="descriptionbox-description-empty">No description provided.</p>
+          )
+        }
       </div>
+      {
+        details && (
+          <button className="descriptionbox-details-button" onClick={onSeeDetailsClick}>See details</button>
+        )
+      }
     </div>
   )
 }
 
 export default DescriptionBox
+
+
+import React from 'react'
+import DescriptionBox from './DescriptionBox'
+
+const App = () => {
+  const [showDetails, setShowDetails] = React.useState(false)
+
+  const handleSeeDetailsClick = () => {
+    set
