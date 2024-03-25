5664ec86d0701d673ba6fa4380a9db43bd95b0d7
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:16 +0100

    improve

--- liguedelorainne/admin/src/App.js
+++ liguedelorainne/admin/src/App.js
@@ -1,18 +1,30 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Suspense } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Navbar />
-        <Admin />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Admin} />
+            <Route path="/admin" component={Admin} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
         <Footer />
       </div>
+      <Helmet>
+        <title>My App</title>
+      </Helmet>
     </BrowserRouter>
   );
 }
 
 export default App;
+
