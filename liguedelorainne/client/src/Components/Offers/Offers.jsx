6b7cc3ca8ee1afa5a944f31db18873498d3410f6
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:27 +0100

    improve

--- liguedelorainne/client/src/Components/Offers/Offers.jsx
+++ liguedelorainne/client/src/Components/Offers/Offers.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Offers.css";
-import exclusive_image from "../Assets/exclusive_image.png";
+import exclusiveImage from "../Assets/exclusive_image.png"; // use destructuring to rename the import
 
 const Offers = () => {
   return (
     <div className="offers">
       <div className="offers-left">
-        <h1>Exclusive</h1>
-        <h1>Offers For You</h1>
-        <p>ONLY ON BEST SELLERS PRODUCTS</p>
-        <button>Check now</button>
+        <h1 className="offers-title">Exclusive</h1>
+        <h1 className="offers-title">Offers For You</h1>
+        <p className="offers-subtitle">ONLY ON BEST SELLERS PRODUCTS</p>
+        <button className="offers-button" type="button">
+          Check now
+        </button>
       </div>
       <div className="offers-right">
-        <img src={exclusive_image} alt="" />
+        <img src={exclusiveImage} alt="An exclusive image representing the offer" />
       </div>
     </div>
   );
+};
+
+Offers.propTypes = {
+  image: PropTypes.string.isRequired,
 };
 
 export default Offers;
