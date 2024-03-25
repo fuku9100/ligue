51782e77af219b3a1ddeb41f5c4a3475e78c44b4
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:28 +0100

    improve

--- liguedelorainne/client/src/index.js
+++ liguedelorainne/client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ShopContextProvider from './Context/ShopContext';
+import ShopContext from './Context/ShopContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a constant for the root element
+const rootElement = document.getElementById('root');
+
+// Use destructuring to get the createRoot method from ReactDOM
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the App component wrapped in the ShopContextProvider
 root.render(
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
+  <ShopContext.Provider>
+    <App />
+  </ShopContext.Provider>
 );
