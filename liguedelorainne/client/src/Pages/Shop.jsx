38b6bbbb6a7ca740479a52c5f0deba3fc074304e
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:30 +0100

    improve

--- liguedelorainne/client/src/Pages/Shop.jsx
+++ liguedelorainne/client/src/Pages/Shop.jsx
@@ -6,33 +6,10 @@ import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 const Shop = () => {
+  const [popular, setPopular] = useState<any[]>([]);
+  const [newcollection, setNewCollection] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  const [popular, setPopular] = useState([]);
-  const [newcollection, setNewCollection] = useState([]);
-
-  const fetchInfo = () => { 
-    fetch('http://localhost:4000/popularinwomen') 
-            .then((res) => res.json()) 
-            .then((data) => setPopular(data))
-    fetch('http://localhost:4000/newcollections') 
-            .then((res) => res.json()) 
-            .then((data) => setNewCollection(data))
-    }
-
-    useEffect(() => {
-      fetchInfo();
-    }, [])
-
-
-  return (
-    <div>
-      <Hero/>
-      <Popular data={popular}/>
-      <Offers/>
-      <NewCollections data={newcollection}/>
-      <NewsLetter/>
-    </div>
-  )
-}
-
-export default Shop
+  const fetchInfo = async () => { 
+    try {
+      const popularRes = await fetch('http://localhost:400
