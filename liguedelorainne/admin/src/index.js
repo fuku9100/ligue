40a486cf7bac6518a8ec8d19750ae296ae1f9091
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:19 +0100

    improve

--- liguedelorainne/admin/src/index.js
+++ liguedelorainne/admin/src/index.js
@@ -1,11 +1,20 @@
+// Import React and ReactDOM, which are required for rendering React components
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// Import the App component, which is the top-level component of the application
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the index.css file, which contains global styles for the application
+import './index.css';
+
+// Create a root ReactDOM element, which will be used to render the App component
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+// Render the App component to the root ReactDOM element
 root.render(
+  <React.StrictMode>
+    {/* Wrap the App component with React.StrictMode to enable additional checks and warnings for development */}
     <App />
+  </React.StrictMode>
 );
-
-
