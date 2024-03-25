c9dd96820d25093f3425ca82ebcb562195dbb36f
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:38 +0100

    improve

--- liguedelorainne/client/src/Components/RelatedProducts/RelatedProducts.jsx
+++ liguedelorainne/client/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -4,14 +4,17 @@ import Item from '../Item/Item'
 import data_product from '../Assets/data'
 
 const RelatedProducts = () => {
+  const relatedProducts = data_product.map(({ id, name, image, new_price, old_price, stock }) => {
+    const price = old_price ? `$${old_price} $${new_price}` : `$${new_price}`;
+    return <Item key={id} id={item.id} name={item.name} image={item.image} price={price} stock={item.stock} />
+  });
+
   return (
     <div className='relatedproducts'>
       <h1>Related Products</h1>
       <hr />
       <div className="relatedproducts-item">
-        {data_product.map((item)=>{
-            return <Item id={item.id} name={item.name} image={item.image}  new_price={item.new_price} old_price={item.old_price} stock={item.stock}/>
-        })}
+        {relatedProducts}
       </div>
     </div>
   )
