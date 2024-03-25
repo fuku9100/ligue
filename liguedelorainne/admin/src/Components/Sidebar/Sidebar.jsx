b1c0b5f14379c02c9111a387fed00f81a99640ee
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:18 +0100

    improve

--- liguedelorainne/admin/src/Components/Sidebar/Sidebar.jsx
+++ liguedelorainne/admin/src/Components/Sidebar/Sidebar.jsx
@@ -7,21 +7,37 @@ import { Link } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <Link to='/addproduct' style={{ textDecoration: 'none' }}>
-        <div className="sidebar-item">
-          <img src={add_product_icon} alt="" />
-          <p>Add Product</p>
-        </div>
-      </Link>
-      <Link to='/listproduct' style={{ textDecoration: 'none' }}>
-        <div className="sidebar-item">
-          <img src={list_product_icon} alt="" />
-          <p>Product List</p>
-        </div>
-      </Link>
-      
+      <div className="sidebar-container">
+        <Link
+          to={'/addproduct'}
+          style={{ textDecoration: 'none' }}
+          activeClassName="active"
+          key={'add-product'}
+        >
+          <div className="sidebar-item">
+            <img src={add_product_icon} alt="Add Product" />
+            <p>Add Product</p>
+          </div>
+        </Link>
+        <Link
+          to={'/listproduct'}
+          style={{ textDecoration: 'none' }}
+          activeClassName="active"
+          key={'product-list'}
+        >
+          <div className="sidebar-item">
+            <img src={list_product_icon} alt="Product List" />
+            <p>Product List</p>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
 
 export default Sidebar
+
+
+.sidebar .sidebar-container .active {
+  background-color: #f5f5f5;
+}
