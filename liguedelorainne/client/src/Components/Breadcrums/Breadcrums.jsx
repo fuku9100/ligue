880d4f3bb9f3e50ba4ae40f5f2dadaa473f18332
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:32 +0100

    improve

--- liguedelorainne/client/src/Components/Breadcrums/Breadcrums.jsx
+++ liguedelorainne/client/src/Components/Breadcrums/Breadcrums.jsx
@@ -3,10 +3,24 @@ import './Breadcrums.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 const Breadcrums = (props) => {
-  const {product} = props;
+  const { product } = props;
   return (
     <div className='breadcrums'>
-      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name} 
+      <span title='Home page'>
+        <img src={arrow_icon} alt="Home icon" /> HOME
+      </span>
+      <img src={arrow_icon} alt="Arrow icon" />
+      <span title='Shop page'>
+        <img src={arrow_icon} alt="Shop icon" /> SHOP
+      </span>
+      <img src={arrow_icon} alt="Arrow icon" />
+      <span title={`${product.category} page`}>
+        {product.category}
+      </span>
+      <img src={arrow_icon} alt="Arrow icon" />
+      <span title={`${product.name} page`}>
+        {product.name}
+      </span>
     </div>
   )
 }
