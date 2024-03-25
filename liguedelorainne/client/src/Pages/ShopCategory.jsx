609ce9370f2c88f937d6ead016c6a03ef5274d9a
fuku9100 <116653740+fuku9100@users.noreply.github.com>
2024-03-25 16:26:29 +0100

    improve

--- liguedelorainne/client/src/Pages/ShopCategory.jsx
+++ liguedelorainne/client/src/Pages/ShopCategory.jsx
@@ -1,47 +1,52 @@
 import React, { useEffect, useState } from "react";
 import "./CSS/ShopCategory.css";
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
 import Item from "../Components/Item/Item";
 import { Link } from "react-router-dom";
 
 const ShopCategory = (props) => {
-
   const [allproducts, setAllProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const itemsToDisplay = 12;
 
-  const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
-            .then((res) => res.json()) 
-            .then((data) => setAllProducts(data))
-    }
+  const fetchInfo = () => {
+    fetch("http://localhost:4000/allproducts")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setAllProducts(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  };
 
-    useEffect(() => {
-      fetchInfo();
-    }, [])
-    
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const displayedItems = allproducts.slice(0, itemsToDisplay);
+
   return (
     <div className="shopcategory">
-      <img src={props.banner} className="shopcategory-banner" alt="" />
+      <img src={props.banner} className="shopcategory-banner" alt="Shop banner" />
       <div className="shopcategory-indexSort">
-        <p><span>Showing 1 - 12</span> out of 54 Products</p>
-        <div className="shopcategory-sort">Sort by  <img src={dropdown_icon} alt="" /></div>
+        <p>
+          <span>{`Showing 1 - ${itemsToDisplay}`}</span> out of {allproducts.length}{" "}
+          Products
+        </p>
+        <div className="shopcategory-sort">Sort by <img src={dropdown_icon} alt="Sort icon" /></div>
       </div>
       <div className="shopcategory-products">
-        {allproducts.map((item,i) => {
-            if(props.category===item.category)
-            {
-              return <Item id={item.id} key={i} name={item.name} image={item.image}  new_price={item.new_price} old_price={item.old_price} stock={item.stock}/>;
-            }
-            else
-            {
-              return null;
-            }
-        })}
-      </div>
-      <div className="shopcategory-loadmore">
-      <Link to='/' style={{ textDecoration: 'none' }}>Explore More</Link>
-      </div>
-    </div>
-  );
-};
-
-export default ShopCategory;
+        {loading && <p>Loading products...</p>}
+        {error && <p>Error: {error}</p>}
+        {!loading &&
+          allproducts.length > 0 &&
+          displayed
